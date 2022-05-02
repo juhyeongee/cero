e9a7c0a0609a46742f498d95e6a41a172d2b358a
juhyeong <juhyeongee@gmail.com>
2022-05-02 15:52:59 +0900

    feat: 스플래시 화면 컴포넌트 생성

---
+++ screens/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const Splash = () => {
+  return (
+    <View>
+      <Text>Splash Page</Text>
+    </View>
+  );
+};
+
+export default Splash;
