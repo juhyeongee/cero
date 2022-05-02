a1cf597875162b4f27d18b6c285618714b5cb0aa
juhyeong <juhyeongee@gmail.com>
2022-05-02 15:48:19 +0900

    feat: 일자별 질문데이터 추가

---
+++ screens/components/Missions.js
@@ -0,0 +1,16 @@
+import react, { useState } from "react";
+
+let day_1 = {
+  day: 1,
+  subTitle: " 스스로를 위한 선물",
+  contents:
+    " 고생한 당신, 오늘만큼은 폭주해보는게 어떨까요. 정말 그간 갖고싶었던 것 하나를 스스로에게 선물하는 거예요 ",
+};
+let day_2 = {
+  day: 2,
+  subTitle: " 하늘 사진 찍기",
+  contents: " 맑은 하늘 사진을 한 번 찍어보아요. ",
+};
+const missionArray = [day_1, day_2];
+
+export default missionArray;
