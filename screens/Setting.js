fb8f629b0457c1be2be8e8be6024a125e3dce0d5
juhyeong <juhyeongee@gmail.com>
2022-05-08 04:33:37 +0900

    refactor: 로그아웃 버튼 수정

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
 import styled from "styled-components/native";
 
-const Setting = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>설정페이지</Text>
-  </View>
-);
+const LogoutBtn = styled.Button`
+  border: 1px solid black;
+  width: 50%;
+  height: 10%;
+`;
+const Setting = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 0.5 }}>
+        <LogoutBtn
+          title="로그아웃"
+          onPress={() =>
+            auth()
+              .signOut()
+              .then(() => console.log("로그아웃 되었습니다"))
+          }
+        >
+          <Text>설정페이지</Text>
+        </LogoutBtn>
+      </View>
+    </View>
+  );
+};
 export default Setting;
