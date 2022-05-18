2d405cf9dd2da9437dd86ce3141cd6ec87fca4da
juhyeong <juhyeongee@gmail.com>
2022-05-18 12:41:16 +0900

    Feat: 심리테스트 결과 AsyncStorage 저장,

--- screens/components/mindTest/MindTestBody.js
+++ screens/components/mindTest/MindTestBody.js
@@ -2,51 +2,62 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 import questionObj from "../Qustions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Body = (props) => {
-  const BodyBG = styled.View`
-    width: 100%;
-    flex: 0.6;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const Question = styled.Text`
-    font-size: 22px;
-    margin: 20px;
-    margin-bottom: 90px; ;
-  `;
-  const AnswerButton = styled.TouchableOpacity`
-    width: 80%;
-    height: 60px;
-    padding: 20px;
-    background-color: ${(props) => props.theme.mainBlackColor};
-    justify-content: center;
-    border-radius: 10px;
-    margin: 4px;
-  `;
-  const AnswerButtonText = styled.Text`
-    font-size: 16px;
-    color: ${(props) => props.theme.bgColor};
-  `;
+  const recordAnswer = (params) => {
+    AsyncStorage.setItem(`answer${props.pageNumber}`, `${params}`);
+    AsyncStorage.getItem(`answer${props.pageNumber}`).then((text) =>
+      console.log(text)
+    );
+    AsyncStorage.getAllKeys().then((text) => {
+      console.log(text);
+    });
+  };
 
   return (
     <BodyBG>
       <Question>{questionObj[props.pageNumber]}</Question>
-      <AnswerButton>
+      <AnswerButton onPress={() => recordAnswer(1)}>
         <AnswerButtonText>극히 드물게</AnswerButtonText>
       </AnswerButton>
-      <AnswerButton>
+      <AnswerButton onPress={() => recordAnswer(2)}>
         <AnswerButtonText>가끔 (1~2일)</AnswerButtonText>
       </AnswerButton>
-      <AnswerButton>
+      <AnswerButton onPress={() => recordAnswer(3)}>
         <AnswerButtonText>자주 (3~4일)</AnswerButtonText>
       </AnswerButton>
-      <AnswerButton>
+      <AnswerButton onPress={() => recordAnswer(4)}>
         <AnswerButtonText>거의 대부분 (5~7일)</AnswerButtonText>
       </AnswerButton>
     </BodyBG>
   );
 };
+
+const BodyBG = styled.View`
+  width: 100%;
+  flex: 0.6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Question = styled.Text`
+  font-size: 22px;
+  margin: 20px;
+  margin-bottom: 90px; ;
+`;
+const AnswerButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 60px;
+  padding: 20px;
+  background-color: ${(props) => props.theme.mainBlackColor};
+  justify-content: center;
+  border-radius: 10px;
+  margin: 4px;
+`;
+const AnswerButtonText = styled.Text`
+  font-size: 16px;
+  color: ${(props) => props.theme.bgColor};
+`;
 
 export default Body;
---
+++ screens/components/mindTest/MindTestIntroduce.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+import questionObj from "../Qustions";
+
+const MindTestIntroduce = (props) => {
+  const BG = styled.View`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  return (
+    <BG>
+      <Text>
+        본격적인 여정을 시작하기 전, OO님의 마음 상태를 알아보기 위해 간단한
+        질문을 준비했어요.
+      </Text>
+      <TouchableOpacity onPress={props.setIntroduceBtn}>
+        <Text>다음</Text>
+      </TouchableOpacity>
+    </BG>
+  );
+};
+export default MindTestIntroduce;
