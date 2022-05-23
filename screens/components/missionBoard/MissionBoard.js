2bcc5bdd1de570b1c43725adbd7fc9bbc6288eef
juhyeong <juhyeongee@gmail.com>
2022-05-23 15:40:28 +0900

    Feat: 미션 페이지, 주의사항 팝업 추가

--- screens/components/missionBoard/MissionBoard.js
+++ screens/components/missionBoard/MissionBoard.js
@@ -1,64 +1,112 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components/native";
 import missionObj from "../Missions";
-
-const TodayMission = (props) => {
-  return (
-    <View
-      style={{
-        flex: 0.2,
-        backgroundColor: "teal",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 16 }}>
-        Q.{props.missionObj.day1.version1.subtitle}
-      </Text>
-    </View>
-  );
-};
-
-const HowToDo = (props) => {
-  return (
-    <View
-      style={{
-        padding: 20,
-        flex: 0.4,
-        backgroundColor: "tomato",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 13 }}>
-        How to do. {props.missionObj.day1.version1.contents}
-      </Text>
-    </View>
-  );
-};
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from "../property";
 
 const MissionBoard = (props) => {
-  const [text, setText] = useState("Write your action");
+  const [text, setText] = useState("");
   const [todayMission, setTodayMission] = useState();
+  const [showPrecautious, setShowPrecautious] = useState(false);
 
   return (
     <View style={styles.board}>
       <Header>
         <Text style={styles.title}>Today's mission </Text>
       </Header>
-      <TodayMission missionObj={missionObj} />
-      <HowToDo missionObj={missionObj} />
-      <TextInput
-        style={{ flex: 1, padding: 20 }}
-        onChangeText={setText}
-        value={text}
-      />
+      {!showPrecautious ? (
+        <PrecautiousBtn
+          onPress={() => {
+            setShowPrecautious(true);
+          }}
+        >
+          <PrecautiousText>주의사항</PrecautiousText>
+        </PrecautiousBtn>
+      ) : (
+        <>
+          <Wall onPress={() => setShowPrecautious(false)} />
+          <PrecautiousPopUp />
+        </>
+      )}
+
+      <View>
+        <Text>Q.{missionObj.day1.version1.subtitle}</Text>
+      </View>
+      <View>
+        <Text>How to do. {missionObj.day1.version1.contents}</Text>
+      </View>
+      {!showPrecautious ? (
+        <TextInput
+          placeholder="오늘의 미션을 적어주세요"
+          style={{ flex: 1, padding: 20 }}
+          onChangeText={setText}
+          value={text}
+        />
+      ) : (
+        <View style={{ flex: 1, padding: 20 }} />
+      )}
+
+      <CompleteBtn
+        onPress={() => {
+          console.log("제출 완료");
+        }}
+      >
+        <CompleteBtnText>완료 버튼</CompleteBtnText>
+      </CompleteBtn>
     </View>
   );
 };
+
+const PrecautiousPopUp = () => {
+  const PopUpFrame = styled.View`
+    background-color: aliceblue;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 70%;
+    height: 50%;
+  `;
+  return (
+    <PopUpFrame>
+      <Text>주의사항 팝업</Text>
+      <Text>사진 웃기고이상한거 올리지마라..</Text>
+      <Text>개인정보어쩌고..</Text>
+    </PopUpFrame>
+  );
+};
+const PrecautiousBtn = styled.TouchableOpacity`
+  font-size: 20;
+`;
+
+const Wall = styled.TouchableOpacity`
+  position: absolute;
+  background-color: black;
+  opacity: 0.4;
+  width: ${(props) => props.theme.windowWidth};
+  height: ${(props) => props.theme.windowHeight};
+`;
+
+const PrecautiousText = styled.Text``;
+
+const CompleteBtn = styled.TouchableOpacity`
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200;
+  height: 100;
+`;
+const CompleteBtnText = styled.Text`
+  color: white;
+`;
 
 const Header = styled.View`
   flex-direction: row;
@@ -70,9 +118,10 @@ const Header = styled.View`
 
 const styles = StyleSheet.create({
   board: {
-    flex: 0.7,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
     backgroundColor: "bisque",
-    width: "80%",
     borderRadius: 30,
   },
   title: {
--- screens/components/theme.js
+++ screens/components/theme.js
@@ -1,6 +1,10 @@
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from "./property";
+
 export const mainTheme = {
   mainBlackColor: "#555555",
   bgColor: "#faf8f4",
+  windowWidth: WINDOW_WIDTH,
+  windowHeight: WINDOW_HEIGHT,
 };
 
 export const darkTheme = {};
