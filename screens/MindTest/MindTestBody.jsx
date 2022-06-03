1d694cefaa2d0b7771a74aa872669e2897327a0e
juhyeong <juhyeongee@gmail.com>
2022-06-03 15:26:53 +0900

    Design: MindTest 화면 디자인 컴포넌트 적용

--- screens/MindTest/MindTestBody.jsx
+++ screens/MindTest/MindTestBody.jsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import questionObj from "../../constants/Qustions";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import AnswerBtn from "./AnswerBtn";
+import { Layout, OnOffButton } from "../components";
+
 const Body = (props) => {
   const [clickedBtnNumber, setClickedBtnNumber] = useState(0);
   //clickedBtnNumber은 방금 눌린 번호가 뭔지 기록해놓는 역할
@@ -11,6 +13,7 @@ const Body = (props) => {
   const onHandleClickBtnNumber = (btnNumber) => {
     setClickedBtnNumber(btnNumber);
     saveAnswer(btnNumber);
+    setTimeout(() => this._swiper.scrollBy(1), 200);
   };
 
   const saveAnswer = (clickedBtnNumber) => {
@@ -19,36 +22,48 @@ const Body = (props) => {
   };
 
   return (
-    <BodyBG>
-      <LastWeekText>최근 일주일 동안...</LastWeekText>
-      <Question>{questionObj[props.pageNumber]}</Question>
-      <AnswerBtn
-        clickedBtnNumber={clickedBtnNumber}
-        onHandleClickBtnNumber={() => onHandleClickBtnNumber(1)}
-        number={1}
-        content="극히 드물게"
-      />
-      <AnswerBtn
-        clickedBtnNumber={clickedBtnNumber}
-        onHandleClickBtnNumber={() => onHandleClickBtnNumber(2)}
-        number={2}
-        content="가끔 (1~2일)"
-      />
-      <AnswerBtn
-        clickedBtnNumber={clickedBtnNumber}
-        onHandleClickBtnNumber={() => onHandleClickBtnNumber(3)}
-        number={3}
-        content="자주 (3~4일)"
-      />
-      <AnswerBtn
-        clickedBtnNumber={clickedBtnNumber}
-        onHandleClickBtnNumber={() => onHandleClickBtnNumber(4)}
-        number={4}
-        content="거의 대부분 (5~7일)"
-      />
-    </BodyBG>
+    <>
+      <ContentContainer>
+        <LastWeekText>최근 일주일 동안...</LastWeekText>
+        <Question>{questionObj[props.pageNumber]}</Question>
+      </ContentContainer>
+      <ButtonContainer>
+        <AnswerBtn
+          clickedBtnNumber={clickedBtnNumber}
+          onHandleClickBtnNumber={() => onHandleClickBtnNumber(1)}
+          number={1}
+          content="극히 드물게"
+        />
+        <AnswerBtn
+          clickedBtnNumber={clickedBtnNumber}
+          onHandleClickBtnNumber={() => onHandleClickBtnNumber(2)}
+          number={2}
+          content="가끔 (1~2일)"
+        />
+        <AnswerBtn
+          clickedBtnNumber={clickedBtnNumber}
+          onHandleClickBtnNumber={() => onHandleClickBtnNumber(3)}
+          number={3}
+          content="자주 (3~4일)"
+        />
+        <AnswerBtn
+          clickedBtnNumber={clickedBtnNumber}
+          onHandleClickBtnNumber={() => onHandleClickBtnNumber(4)}
+          number={4}
+          content="거의 대부분 (5~7일)"
+        />
+      </ButtonContainer>
+    </>
   );
 };
+
+const ContentContainer = styled.View`
+  justify-content: flex-end;
+  flex: 1;
+`;
+const ButtonContainer = styled.View`
+  flex: 2;
+`;
 
 const BodyBG = styled.View`
   width: 100%;
--- screens/components/OutlineButton.jsx
+++ screens/components/OutlineButton.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, Pressable } from "react-native";
 import styled from "styled-components";
 
-export const OutlineButton = (props) => {
-  const OutlineButtonBG = styled.Pressable`
-    border: 1px solid ${(props) => props.theme.n700};
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    background-color: ${(props) => props.theme.n100};
-    border-radius: 10px;
-    height: 48px;
-    padding: 10px 20px;
-    margin-top: 10px;
-  `;
-  const OutlineButtonText = styled.Text`
-    font-family: ${(props) => props.theme.mainFont};
-    font-size: 16px;
-    color: ${(props) => props.theme.n900};
-  `;
+const OutlineButton = (props) => {
   return (
     <OutlineButtonBG onPress={props.onPress}>
       <OutlineButtonText>{props.text}</OutlineButtonText>
     </OutlineButtonBG>
   );
 };
+const OutlineButtonBG = styled.Pressable`
+  border: 1px solid ${(props) => props.theme.n900};
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: ${(props) => props.theme.n100};
+  border-radius: 10px;
+  height: 48px;
+  padding: 10px 20px;
+  margin-top: 10px;
+`;
+const OutlineButtonText = styled.Text`
+  font-family: ${(props) => props.theme.mainFont};
+  font-size: 16px;
+  color: ${(props) => props.theme.n900};
+`;
+
+export default OutlineButton;
