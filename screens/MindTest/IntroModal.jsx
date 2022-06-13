ba38a96aa9714eea55dfdda02286c4ab39f789a9
juhyeong <juhyeongee@gmail.com>
2022-06-13 22:38:44 +0900

    Feat: MindTest전 소개화면 & 모달창 추가

---
+++ screens/MindTest/IntroModal.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, Modal } from "react-native";
+import styled from "styled-components";
+
+const IntroModal = (props) => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  //그냥 갑자기 이렇게도 쓰고싶어서..
+  const text = {
+    0: "좋아요, 지금부터 효준님의 마음을 살펴보겠습니다.",
+    1: "다음 질문에 답변해주시면, 효준님의 마음이 씨앗이 되어 화분에 심어지게 됩니다.",
+    2: "앞으로 14일간,  이 씨앗을 멋지게 키워주세요.",
+    3: "씨앗이 움을 틔울수록, 효준님의 마음도 점점 근사하게 성장하게 될 거예요.",
+  };
+  console.log("페이지넘버 :" + pageNumber);
+  return (
+    <ShortModal>
+      <ModalText>{text[pageNumber]}</ModalText>
+      {pageNumber !== 3 ? (
+        <NextButton onPress={() => setPageNumber(pageNumber + 1)}>
+          <ButtonText>클릭하여 다음으로</ButtonText>
+        </NextButton>
+      ) : (
+        <NextButton onPress={() => props.setMindSeedModalVisible(true)}>
+          <ButtonText>클릭해서 다음으로</ButtonText>
+        </NextButton>
+      )}
+    </ShortModal>
+  );
+};
+
+const ShortModal = styled.View`
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.n300};
+  position: absolute;
+  bottom: 0px;
+  height: 27%;
+  width: 100%;
+  padding: 10%;
+`;
+
+const ModalText = styled.Text`
+  font-family: ${(props) => props.theme.mainFont};
+  font-size: 18px;
+`;
+
+const NextButton = styled.Pressable`
+  flex-direction: row-reverse;
+`;
+
+const ButtonText = styled.Text`
+  font-family: ${(props) => props.theme.mainFont};
+  font-size: 16px;
+  color: ${(props) => props.theme.mainGreen};
+`;
+
+export default IntroModal;
--- screens/MindTest/MindTestIntroduce.jsx
+++ screens/MindTest/MindTestIntroduce.jsx
@@ -1,21 +1,63 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Pressable, Modal, Image } from "react-native";
 import styled from "styled-components";
 import questionObj from "../../constants/Qustions";
 import { Layout, MainText, SubText, BigGreenButton } from "../components";
+import IntroModal from "./IntroModal";
 
 const MindTestIntroduce = (props) => {
+  const [mindSeedModalVisible, setMindSeedModalVisible] = useState(false);
   return (
-    <Layout center={true}>
-      <MainText>정말 잘하셨어요! </MainText>
-      <SubText>
-        본격적인 여정을 시작하기 전, OO님의 마음 상태를 알아보기 위해 간단한
-        질문을 준비했어요.
-      </SubText>
-      <View style={{ height: "30%" }}></View>
-      <BigGreenButton text="다음" onPress={props.setIntroduceBtn} />
-    </Layout>
+    <>
+      <Layout center={true}>
+        <MainText>정말 잘하셨어요! </MainText>
+
+        <View style={{ height: "30%" }}></View>
+      </Layout>
+      <IntroModal
+        setIntroduceBtn={props.setIntroduceBtn}
+        setMindSeedModalVisible={setMindSeedModalVisible}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={mindSeedModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          props.setVisible(!visible);
+        }}
+      >
+        <EmptyContainer />
+        <Container>
+          <Image source={require("cero_/assets/seedBox.png")}></Image>
+          <MainText>마음 씨앗 만들기</MainText>
+          <SubText>20개 질문, 평균 5분 소요</SubText>
+          <SubText>
+            지난 일주일을 가만히 돌아보세요. 온전히 나에게 집중할 수 있는 편안한
+            환경에서 함께해시길 추천해요.
+          </SubText>
+
+          <BigGreenButton text="계속하기" onPress={props.setIntroduceBtn} />
+        </Container>
+      </Modal>
+    </>
   );
 };
+
+const EmptyContainer = styled.Pressable`
+  flex: 0.6;
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 0.4;
+  padding: 32px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: ${(props) => props.theme.n100};
+`;
 
 export default MindTestIntroduce;
