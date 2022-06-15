e1e4d59c7d4659be57d9299cb174420f625e98be
juhyeong <juhyeongee@gmail.com>
2022-06-15 14:59:19 +0900

    🚧 useStore에 저장한 변수들이 Promise로 출력되는 기현상.
    비동기로 값을 저장했는데, 안됨

--- screens/StartMission/index.js
+++ screens/StartMission/index.js
@@ -16,12 +16,26 @@ console.log(launchCamera, "||||", launchImageLibrary);
 const StartMission = (props) => {
   const navigation = useNavigation();
   const [letter, setLetter] = useState("");
-  const { day } = useStore();
+  const {
+    day,
+    todayDate,
+    missionCompleteDate,
+    completedMissionNumber,
+    finishMissionAndIncreaseMissionNumber,
+  } = useStore();
   // const result = await launchImageLibrary(options?)
   //visibleMissionScreen 으로 boolean은 변수명을 보자마자 알 수있게 해주는게 좋습니다. 카멜케이스로 씁시다~ is has visible hidden을 사용한다.
   //근본있게 docs로 공부하기.
   // 비동기:  오래걸리는거 나중에 처리해줘. 약속해저. (서버 다녀올때 많이 )
   //2~3초 걸리는 api호출 하고, 다른 작업들을 폰에서 처리해줌.
+  console.log("todayDate", todayDate);
+  console.log("missionCompleteDate", missionCompleteDate);
+  console.log("completedMissionNumber", completedMissionNumber);
+  console.log(
+    "finishMissionAndIncreaseMissionNumber",
+    finishMissionAndIncreaseMissionNumber
+  );
+
   const days = "day" + day;
   const submitTextMission = () => {
     AsyncStorage.setItem("Mission1Submit", letter).then(
@@ -34,7 +48,7 @@ const StartMission = (props) => {
   return (
     <Layout>
       <TitleContainer>
-        <MainText>미션 1: {missionObj[days].version1.subtitle}</MainText>
+        {/* <MainText>미션 1: {missionObj[days].version1.subtitle}</MainText> */}
         <TodaysMission />
       </TitleContainer>
       <SubmitContentsContainer>
